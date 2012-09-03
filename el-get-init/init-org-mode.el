6588bf60f4b48ea1a69b95ca8da5d5e3d8d0374b
Joel Suovaniemi <joel.suovaniemi@iki.fi>
2012-09-03 23:37:39 +0300

    windmove works in org-mode (but not in agenda)

--- el-get-init/init-org-mode.el
+++ el-get-init/init-org-mode.el
@@ -67,6 +67,12 @@
 (define-key org-mode-map "\C-c[" nil)
 (define-key org-mode-map "\C-c]" nil)
 
+;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 (setq org-capture-templates
 '(("t" "Todo" entry (file+headline "" "Tasks")
 	 "* TODO %?\n  %U\n  %a\n  %K\n")
