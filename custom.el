68f2abc6eed9abc15d3d56002d82e0163325a5aa
Joel Suovaniemi <joel.suovaniemi@iki.fi>
2012-09-04 15:25:15 +0300

    locations bookmarks custom

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (sh . t) (ruby . t))))
  '(org-enforce-todo-dependencies t)
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-id org-info org-jsinfo org-habit org-irc org-mac-message org-mew org-mhe org-protocol org-rmail org-vm org-wl org-w3m org-elisp-symbol org-git-link org-mac-iCal org-mac-link-grabber)))
  '(org-refile-use-outline-path (quote file))
