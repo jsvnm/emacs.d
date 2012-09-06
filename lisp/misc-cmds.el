09e7217e377bead7385127dd2979b2742fd75d37
Joel Suovaniemi <joel.suovaniemi@iki.fi>
2012-09-06 19:16:12 +0300

    ssh-status and ssh-add commands to talk with ssh-agent

--- lisp/misc-cmds.el
+++ lisp/misc-cmds.el
@@ -20,3 +20,36 @@
 		(princ (substitute-command-keys
 						(format "description of variable %s\n\\{%s}" kmap kmap)))
 		(help-make-xrefs (help-buffer))))
+
+
+(defun ssh-status ()
+	(interactive)
+	(let ((st (call-process "ssh-add" nil nil nil "-l")))
+		(when (called-interactively-p)
+			(message "ssh-agent %s"
+							 (cdr (assq st '((0 . "has your identity")
+															 (1 . "is running with no identity")
+															 (2 . "is hiding"))))))
+		st))
+
+(defun ssh-add (arg)
+	(interactive "P")
+	(let ((st (ssh-status)))
+		(if (eq 2 st) (message "agent not found")
+			(if (null arg) (if (eq 0 st) (message "no need to add")
+											 (call-process "ssh-add" nil nil nil))
+				(if (eq 1 st) (message "no need to remove")
+					(call-process "ssh-add" nil nil nil "-d"))))))
+
+
+
+
+
+
+
+
+
+
+
+
+
