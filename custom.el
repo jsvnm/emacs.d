16434ec0acb479b140e5f6c79dc2aa426a0fdb98
Joel Suovaniemi <joel.suovaniemi@iki.fi>
2012-11-05 17:41:42 +0200

    updated .gitignore and custom.el

--- custom.el
+++ custom.el
@@ -17,6 +17,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "apple" :family "Monaco"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "outline" :family "Monospac821 BT"))))
  '(org-column ((t (:background "#553322" :strike-through nil :underline nil :slant normal :weight normal :height 120 :family "Monaco"))))
  '(org-mode-line-clock ((t (:background "white" :foreground "black"))) t))
